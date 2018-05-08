3d36ec5e667a415ea53518e60b3cefaee95c56ee
Norbert de Langen <ndelangen@me.com>
2018-05-08 17:41:59 +0200

    FIX linting

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ module.exports = (text, opts) => {
 
 	const contentWidth = widestLine(text) + padding.left + padding.right;
 	const paddingLeft = PAD.repeat(padding.left);
-	const columns = termSize().columns;
+	const {columns} = termSize();
 	let marginLeft = PAD.repeat(margin.left);
 
 	if (opts.float === 'center') {
