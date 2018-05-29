454018269eedb71a6dd3757de67e8dd22d862d34
Areadrill <jackthemotorcycle@gmail.com>
2018-05-29 05:22:53 +0100

    Add condition to check an overflow will happen and use wrap-ansi. Also add method to correctly split a string with embedded ANSI codes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cliBoxes = require('cli-boxes');
 const camelCase = require('camelcase');
 const ansiAlign = require('ansi-align');
 const termSize = require('term-size');
+const wrapAnsi = require('wrap-ansi');
+const hasAnsi = require('has-ansi')
+const ansi_Split = require('ansi-split')
 
 const getObject = detail => {
 	let object;
@@ -65,6 +68,46 @@ const isHex = color => color.match(/^#[0-f]{3}(?:[0-f]{3})?$/i);
 const isColorValid = color => typeof color === 'string' && ((chalk[color]) || isHex(color));
 const getColorFn = color => isHex(color) ? chalk.hex(color) : chalk[color];
 const getBGColorFn = color => isHex(color) ? chalk.bgHex(color) : chalk[camelCase(['bg', color])];
+const ansiSplit = text => {
+	let lines = []
+	let currStr = ""
+	let resetAnsi = '\u001b[39m'
+	let lastAnsi = resetAnsi
+
+	for(const el of ansi_Split(text)){
+		if(el === ""){
+			continue
+		}
+
+		if(hasAnsi(el)){
+			const ansiCodes = el.split("m")
+			lastAnsi = ansiCodes[ansiCodes.length - 2]
+			currStr += lastAnsi + 'm'
+		}
+		else{
+			let nlIndex
+			let txt = el
+			do{
+				nlIndex = txt.indexOf("\n")
+				if(nlIndex === -1){
+					currStr += txt
+				}
+				else{
+					currStr += txt.slice(0, nlIndex) + resetAnsi
+					lines.push(currStr)
+					currStr = ""
+					currStr += lastAnsi + 'm'
+					txt = txt.slice(nlIndex+1)
+				}
+			}while(nlIndex !== -1)
+		}
+	}
+	lines.push(currStr)
+
+	return lines;
+}
+
+const getBackgroundColorName = x => camelCase('bg', x);
 
 module.exports = (text, options) => {
 	options = {
@@ -100,7 +143,7 @@ module.exports = (text, options) => {
 	const NL = '\n';
 	const PAD = ' ';
 
-	let lines = text.split(NL);
+	let lines = hasAnsi(text) ?  ansiSplit(text) : text.split(NL);
 
 	if (padding.top > 0) {
 		lines = new Array(padding.top).fill('').concat(lines);
@@ -110,10 +153,19 @@ module.exports = (text, options) => {
 		lines = lines.concat(new Array(padding.bottom).fill(''));
 	}
 
-	const contentWidth = widestLine(text) + padding.left + padding.right;
+	let contentWidth = widestLine(text) + padding.left + padding.right;
 	const paddingLeft = PAD.repeat(padding.left);
 	const {columns} = termSize();
 	let marginLeft = PAD.repeat(margin.left);
+
+	if(columns < contentWidth + margin.right + margin.left){
+		const newWidestLine = widestLine(text) - (contentWidth + margin.right + margin.left - columns);
+		const wrapText = wrapAnsi(text, newWidestLine, {hard: true});
+		lines = new Array(padding.top).fill('')
+				.concat(hasAnsi(wrapText) ?  ansiSplit(wrapText) : text.split(NL))
+				.concat(new Array(padding.bottom).fill(''));
+		contentWidth = widestLine(wrapText) + padding.left + padding.right;
+	}
 
 	if (options.float === 'center') {
 		const padWidth = Math.max((columns - contentWidth) / 2, 0);
@@ -129,6 +181,8 @@ module.exports = (text, options) => {
 	const side = colorizeBorder(chars.vertical);
 
 	const middle = lines.map(line => {
+		console.log(line)
+		console.log(contentWidth - stringWidth(line) - padding.left);
 		const paddingRight = PAD.repeat(contentWidth - stringWidth(line) - padding.left);
 		return marginLeft + side + colorizeContent(paddingLeft + line + paddingRight) + side;
 	}).join(NL);
