2e9e41350c91414d5d06b1d3f113431249fa4ca7
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-01 11:58:26 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,77 +1,77 @@
-export interface Options {
-	/**
-	 * Color of the box border.
-	 */
-	borderColor?: string;
-
-	/**
-	 * Style of the box border.
-	 *
-	 * @default `single`
-	 */
-	borderStyle?: string | BorderStyle;
-
-	/**
-	 * Reduce opacity of the border.
-	 *
-	 * @default: false
-	 */
-	dimBorder?: boolean;
-
-	/**
-	 * Space between the text and box border.
-	 *
-	 * @default: 0
-	 */
-	padding?: number | Spacing;
-
-	/**
-	 * Space around the box.
-	 *
-	 * @default 0
-	 */
-	margin?: number | Spacing;
-
-	/**
-	 * Float the box on the available terminal screen space.
-	 *
-	 * @default `left`
-	 */
-	float?: string;
-
-	/**
-	 * Color of the background.
-	 */
-	backgroundColor?: string;
-
-	/**
-	 * Align the text in the box based on the widest line.
-	 *
-	 * @default `left`
-	 */
-	align?: string;
-}
-
 /**
  * Style of the box border.
  */
 export interface BorderStyle {
-	topLeft: string;
-	topRight: string;
-	bottomLeft: string;
-	bottomRight: string;
-	horizontal: string;
-	vertical: string;
+	readonly topLeft: string;
+	readonly topRight: string;
+	readonly bottomLeft: string;
+	readonly bottomRight: string;
+	readonly horizontal: string;
+	readonly vertical: string;
 }
 
 /**
  * Spacing used for `padding` and `margin`.
  */
 export interface Spacing {
-	top: number;
-	right: number;
-	bottom: number;
-	left: number;
+	readonly top: number;
+	readonly right: number;
+	readonly bottom: number;
+	readonly left: number;
+}
+
+export interface Options {
+	/**
+	 * Color of the box border.
+	 */
+	readonly borderColor?: string;
+
+	/**
+	 * Style of the box border.
+	 *
+	 * @default 'single'
+	 */
+	readonly borderStyle?: string | BorderStyle;
+
+	/**
+	 * Reduce opacity of the border.
+	 *
+	 * @default: false
+	 */
+	readonly dimBorder?: boolean;
+
+	/**
+	 * Space between the text and box border.
+	 *
+	 * @default: 0
+	 */
+	readonly padding?: number | Spacing;
+
+	/**
+	 * Space around the box.
+	 *
+	 * @default 0
+	 */
+	readonly margin?: number | Spacing;
+
+	/**
+	 * Float the box on the available terminal screen space.
+	 *
+	 * @default 'left'
+	 */
+	readonly float?: string;
+
+	/**
+	 * Color of the background.
+	 */
+	readonly backgroundColor?: string;
+
+	/**
+	 * Align the text in the box based on the widest line.
+	 *
+	 * @default 'left'
+	 */
+	readonly align?: string;
 }
 
 /**
@@ -82,7 +82,7 @@ declare const _borderStyles: BorderStyle[];
 /**
  * Creates a box in the terminal.
  *
- * @param input - Text inside the box
- * @returns Box containing text.
+ * @param text - The text inside the box.
+ * @returns The box.
  */
-export default function boxen(input: string, options?: Options): string;
+export default function boxen(text: string, options?: Options): string;
