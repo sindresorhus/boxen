5da7c20a6fe58c29d2c751e5317260a73227f331
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-12 19:53:43 +0700

    Improve the colors in the example

--- example.js
+++ example.js
@@ -2,27 +2,27 @@
 const chalk = require('chalk');
 const boxen = require('.');
 
-console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
+console.log('\n\n' + boxen(chalk.cyan('unicorn'), {
 	padding: 1,
 	margin: 1,
 	borderColor: 'yellow'
 }) + '\n');
 
-console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
+console.log('\n\n' + boxen(chalk.cyan('unicorn'), {
 	padding: 1,
 	margin: 1,
 	borderColor: 'yellow',
 	borderStyle: 'double'
 }) + '\n');
 
-console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
+console.log('\n\n' + boxen(chalk.cyan('unicorn'), {
 	padding: 1,
 	margin: 1,
 	borderColor: '#eebbaa',
 	borderStyle: 'double'
 }) + '\n');
 
-console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
+console.log('\n\n' + boxen(chalk.black('unicorn'), {
 	padding: 1,
 	margin: 1,
 	borderColor: '#ffc0cb',
@@ -30,7 +30,7 @@ console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
 	borderStyle: 'double'
 }) + '\n');
 
-console.log('\n\n' + boxen(chalk.blue.bold('unicorn'), {
+console.log('\n\n' + boxen(chalk.black('unicorn'), {
 	padding: 1,
 	margin: 1,
 	borderColor: 'yellow',
