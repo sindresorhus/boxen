20273808df88cbf82f59b0ee5693f68ecaf301da
Caesarovich <cesar@caesarovich.xyz>
2021-09-08 00:26:44 +0200

    Place the constant variables back on top

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const camelCase = require('camelcase');
 const ansiAlign = require('ansi-align');
 const wrapAnsi = require('wrap-ansi');
 
+const NL = '\n';
+const PAD = ' ';
+
 const terminalColumns = () => {
 	const {env, stdout, stderr} = process;
 
@@ -101,9 +104,6 @@ const makeTitle = (text, horizontal, alignement) => {
 };
 
 const makeContentText = (text, padding, columns, align) => {
-	const NL = '\n';
-	const PAD = ' ';
-
 	text = ansiAlign(text, {align});
 	let lines = text.split(NL);
 	const textWidth = widestLine(text);
@@ -193,8 +193,6 @@ module.exports = (text, options) => {
 	};
 
 	const BORDERS_WIDTH = 2;
-	const NL = '\n';
-	const PAD = ' ';
 
 	if (options.borderColor && !isColorValid(options.borderColor)) {
 		throw new Error(`${options.borderColor} is not a valid borderColor`);
